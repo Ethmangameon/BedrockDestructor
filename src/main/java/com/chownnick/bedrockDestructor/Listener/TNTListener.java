75e54e6319d18b12c1299c323c5a9247d981ee9f
Ethan Scott Hedden <ethanhedden0118@gmail.com>
2025-12-11 05:35:05 -0500

    v1.0 Release

--- src/main/java/com/chownnick/bedrockDestructor/Listener/TNTListener.java
+++ src/main/java/com/chownnick/bedrockDestructor/Listener/TNTListener.java
@@ -12,40 +12,59 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.ExplosionPrimeEvent;
 import org.jetbrains.annotations.NotNull;
 
-
 public final class TNTListener implements Listener {
 
     @EventHandler
     public void onTNTExplode(ExplosionPrimeEvent event) {
-        //Get the Entity for the Exploding TNT
+        //Get the entity for the exploding TNT.
         final Entity explodingTNT = event.getEntity();
 
-        //Check if Entity is TNT
-        if (explodingTNT.getType() != EntityType.TNT) {return;}
+        //Check if entity is TNT. (If not return)
+        if (explodingTNT.getType() != EntityType.TNT) return;
 
-        //Get the World of the Exploding TNT
+        //Get the world of the exploding TNT.
         @NotNull World world = explodingTNT.getWorld();
 
-        //Location of TNT
+        //Location of the exploding TNT.
         Location loc = explodingTNT.getLocation();
 
-        //Block of tnt and relative blocks
-        Block Block = world.getBlockAt(loc);
-        Block North = Block.getRelative(BlockFace.NORTH, 1);
-        Block East = Block.getRelative(BlockFace.EAST, 1);
-        Block South = Block.getRelative(BlockFace.SOUTH, 1);
-        Block West = Block.getRelative(BlockFace.WEST, 1);
-        Block Up = Block.getRelative(BlockFace.UP, 1);
-        Block Down = Block.getRelative(BlockFace.DOWN, 1);
+        //Define the block where the tnt is and its relative directly facing blocks.
+        Block blockAtTNT = world.getBlockAt(loc);
+        Block[] allBlocks = new Block[7];
+        for (int i = 0; i < 7; i++) {
+            int faceIndex = (i % 7) + 1;
+            switch (faceIndex) {
+                case 1:
+                    allBlocks[i] = blockAtTNT.getRelative(BlockFace.NORTH, 1);
+                    break;
+                case 2:
+                    allBlocks[i] = blockAtTNT.getRelative(BlockFace.EAST, 1);
+                    break;
+                case 3:
+                    allBlocks[i] = blockAtTNT.getRelative(BlockFace.SOUTH, 1);
+                    break;
+                case 4:
+                    allBlocks[i] = blockAtTNT.getRelative(BlockFace.WEST, 1);
+                    break;
+                case 5:
+                    allBlocks[i] = blockAtTNT.getRelative(BlockFace.UP, 1);
+                    break;
+                case 6:
+                    allBlocks[i] = blockAtTNT.getRelative(BlockFace.DOWN, 1);
+                    break;
+                case 7:
+                    allBlocks[i] = blockAtTNT;
+                    break;
+            }
+        }
 
-        //Checks the relative blocks of TNT and sets them to AIR
-        if (Block.getType() == Material.BEDROCK) {Block.setType(Material.AIR);};
-        if (North.getType() == Material.BEDROCK) {North.setType(Material.AIR);};
-        if (East.getType() == Material.BEDROCK) {East.setType(Material.AIR);};
-        if (South.getType() == Material.BEDROCK) {South.setType(Material.AIR);};
-        if (West.getType() == Material.BEDROCK) {West.setType(Material.AIR);};
-        if (Up.getType() == Material.BEDROCK) {Up.setType(Material.AIR);};
-        if (Down.getType() == Material.BEDROCK) {Down.setType(Material.AIR);};
+
+        //Checks the TNT's position block along with its faces and, sets them to AIR
+        for (Block block : allBlocks) {
+            if (block.getType() == Material.BEDROCK) {
+                block.setType(Material.AIR);
+            }
+        }
 
     }
 
